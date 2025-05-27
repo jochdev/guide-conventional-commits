aab88288534fcddb2976b13d05a20bf2070259e6
José Heredia <jochdev06@gmail.com>
2025-05-27 18:39:52 -0400

    feat: mejora el mensaje de retorno en la función de prueba para mayor claridad

--- app/test-feature.ts
+++ app/test-feature.ts
@@ -1,4 +1,4 @@
 // Archivo de prueba para feat
 export const testFeature = () => {
-  return "Nueva funcionalidad de prueba";
+  return "Nueva funcionalidad de prueba. Esta es una prueba para verificar que la funcionalidad se carga correctamente.";
 };
